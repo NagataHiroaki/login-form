127a80fd7c1d4c8b2d2003403ce4a8b743b1fb4f
NagataHiroaki <ryushion.hyou@gmail.com>
2018-04-05 09:37:05 +0900

    [change] Setted npm scripts

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,4 +1,7 @@
-import * as mdc from 'material-components-web';
+// import * as mdc from 'material-components-web';
 
-const checkbox = new mdc.checkbox.MDCCheckbox(document.querySelector('.mdc-checkbox'));
-const formField = new mdc.formField.MDCFormField(document.querySelector('.mdc-form-field'));
+// const checkbox = new mdc.checkbox.MDCCheckbox(document.querySelector('.mdc-checkbox'));
+// const formField = new mdc.formField.MDCFormField(document.querySelector('.mdc-form-field'));
+
+import {MDCRipple} from '@material/ripple';
+const ripple = new MDCRipple(document.querySelector('.foo-button'));
